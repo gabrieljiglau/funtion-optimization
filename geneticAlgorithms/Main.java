88682c01e76fd87dff2c0b5570ba5d0f18c901eb
jiglau gabriel <100293793+gabrieljiglau@users.noreply.github.com>
2023-11-25 23:46:18 +0200

    Update Main.java

--- geneticAlgorithms/Main.java
+++ geneticAlgorithms/Main.java
@@ -8,8 +8,8 @@ public class Main {
 
         FunctionManager functionManager = new FunctionManager();
 
-        final int PRECISION = 5;
-        final int DIMENSIONS = 5;
+        final int PRECISION = 5; //after decimal point, when converting from binary to decimal
+        final int DIMENSIONS = 5; 
         FunctionDeJongG deJongG = new FunctionDeJongG(DIMENSIONS,PRECISION);
         FunctionSchewefelG schewefelG = new FunctionSchewefelG(DIMENSIONS,PRECISION);
         FunctionMichalewiczG michalewiczG = new FunctionMichalewiczG(DIMENSIONS,PRECISION);
@@ -25,59 +25,14 @@ public class Main {
         functionManager.addFunction(rastriginG);
         functionManager.addFunction(michalewiczG);
 
-        /*GeneticMinima geneticAlgorithm = new GeneticMinima(85, 8.7, 0.004,
-                functionManager,FUNCTION_INDEX_SCHEWEFEL,DIMENSIONS,PRECISION);*/
-
-       /* double startTime = System.currentTimeMillis(); // Restart the timer for each run
+        GeneticMinima geneticAlgorithm = new GeneticMinima(85, 8.7, 0.004,functionManager,FUNCTION_INDEX_SCHEWEFEL,DIMENSIONS,PRECISION);
+        
+        double startTime = System.currentTimeMillis(); 
         System.out.println(geneticAlgorithm.findMinimum(functionManager,FUNCTION_INDEX_RASTRIGIN,DIMENSIONS));
         long finishTime = System.currentTimeMillis();
         double duration =  ((double)(finishTime - startTime) / 1000);
-        System.out.println("Secunde : " + duration);*/
-
-        long startTime;
-        int maxCount = 30;
-        int count = 0;
-        double result = 0;
-        double totalDuration = 0;
-        StringBuilder sb = new StringBuilder(" ");
-
-        double []resultArray = new double[maxCount];
-        double []timeArray = new double[maxCount];
-
-        for(count = 0; count < maxCount; count++) {
-            startTime = System.currentTimeMillis(); // Restart the timer for each run
-            GeneticMinima geneticAlgorithm = new GeneticMinima(85, 8.7, 0.004,
-                    functionManager,FUNCTION_INDEX_SCHEWEFEL,DIMENSIONS,PRECISION);
-
-            double min = geneticAlgorithm.findMinimum(functionManager,FUNCTION_INDEX_SCHEWEFEL,DIMENSIONS);
-            resultArray[count] = min;
-
-            sb.append(count +1).append(".Minimum : ").append(min);
-            long finishTime = System.currentTimeMillis();
-            double duration =  ((double)(finishTime - startTime) / 1000);
-
-            timeArray[count] = duration;
-            result += min;
-            totalDuration += duration;
-
-            sb.append(" duration : ").append(duration).append(" s"); // Print time in seconds
-            sb.append("\n");
-        }
-        try {
-            FileWriter writer = new FileWriter("output.txt");
-            writer.write(sb.toString());
-            writer.close();
-        } catch (IOException e) {
-            System.out.println("An error occurred for FileWriter");
-            e.printStackTrace();
-        }
-
-        double stdevResults = calculateStandardDeviation(resultArray,result / maxCount,maxCount);
-        double stdevTimes = calculateStandardDeviation(timeArray,totalDuration/maxCount,maxCount);
-
-        System.out.println("Average result : " + result / maxCount + "stdevResults : " + stdevResults);
-        System.out.println("Average duration : " + totalDuration / maxCount + " stdevTimes : " + stdevTimes);
-
+        System.out.println("Secunde : " + duration);
+    
     }
 
     public static double calculateStandardDeviation(double[] array,double mean,int length) {
