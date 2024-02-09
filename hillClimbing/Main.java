9fd719a19b5caec32af4f7e2f224281e28422ff8
jiglau gabriel <100293793+gabrieljiglau@users.noreply.github.com>
2024-02-10 00:37:58 +0200

    Update Main.java

--- hillClimbing/Main.java
+++ hillClimbing/Main.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 public class Main {
 
+    private static final Logger LOGGER = Logger.getLogger(SimulatedAnnealing.class.getName());
+
     public static void main(String[] args) {
 
-        HillClimbing algorithm = new HillClimbing();
-
-        int numberOfParameters = 5; //dimensions for the test function
+        int numberOfParameters = 30; //dimensions for the test function
         final int steepness = 10; //one extra parameter for michalewicz
         int precision = 5; //precision after decimal point when converting
 
@@ -19,16 +19,16 @@ public class Main {
 
         int type = 3; //type -> 1 : firstImprovement; 2 : bestImprovement; 3 : worstImprovement
 
-        System.out.println(" " + algorithm.findMinimum(rastrigin,type,numberOfParameters,1500) + " ; ");
+        //LOGGER.info(" " + algorithm.findMinimum(rastrigin,type,numberOfParameters,1500) + " ; ");
 
-        HybridisedSimulatedAnnealing annealing = new HybridisedSimulatedAnnealing(numberOfParameters,3);
+        SimulatedAnnealing annealing = new SimulatedAnnealing(numberOfParameters,20);
 
         long startTime;
         startTime = System.currentTimeMillis();
-        double min = annealing.findMinimumAnnealingHybridised(michalewicz);
+        double min = annealing.findMinimumAnnealingHybridised(schewefel,15000,0.001);
         long finishTime = System.currentTimeMillis();
         double duration =  ((double)(finishTime - startTime) / 1000);
 
-        System.out.println("Min with hybridised SA " + min + " duration " + duration);
+        LOGGER.info("Min with hybridised SA " + min + " duration " + duration);
     }
 }
